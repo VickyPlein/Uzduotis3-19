081395922454e434f9e46f9cb41563d9d096c152
VickyPlein <157711444+VickyPlein@users.noreply.github.com>
2024-03-22 10:38:57 +0200

    pataisymas

--- papildomi/papildoma1.js
+++ papildomi/papildoma1.js
@@ -23,3 +23,43 @@ input.forEach((d) => {
 
 console.log(`Lyginiai skaiciai: ${lyg.join(' ')}`);
 console.log(`Nelyginiai skaiciai: ${nelyg.join(' ')}`);
+
+/*
+const fs = require('fs');
+let data = fs.readFileSync('papildomoseile.txt').toString().split(' ').map(Number);
+
+let even = [];
+let odd = [];
+
+for (let i = 0; i < data.length; i++) {
+    if (data[i] % 2 === 0) {
+        even.push(data[i]);
+    } else {
+        odd.push(data[i]);
+    }
+}
+
+console.log(`Lyginiai skaičiai: ${even.join(' ')} \nNelyginiai skaičiai: ${odd.join(' ')}`);
+*/
+
+/*
+const fs = require('fs');
+let data = fs.readFileSync('papildomoseile.txt').toString().split(" ").map(Number);
+
+console.log(data);
+
+let lyginiai = [];
+let nelyginiai = [];
+
+for (let i = 0; i < data.length; i++) {
+    if (data[i] % 2 === 0) {
+        lyginiai.push(data[i]);
+    } else {
+        nelyginiai.push(data[i]);
+    }
+}
+
+console.log(`Lyginiai skaičiai: ${lyginiai} `);
+console.log(`Nelyginiai skaičiai: ${nelyginiai} `);
+*/
+
